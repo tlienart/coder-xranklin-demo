5adba474ad1528a717e2f4106be65379b9941e82
Thibaut Lienart <tlienart@me.com>
2022-02-20 09:47:46 +0100

    list of blog posts and Hyperscript

--- posts/index.md
+++ posts/index.md
@@ -2,23 +2,4 @@
 header = "Posts"
 +++
 
-~~~
-<ul>
-  <li>
-    <span class="date">March 11, 2019</span>
-    <a class="title" href="/posts/markdown-syntax/">Markdown Syntax Guide</a>
-  </li>
-  <li>
-    <span class="date">March 9, 2019</span>
-    <a class="title" href="/posts/placeholder-text/">Placeholder Text</a>
-  </li>
-  <li>
-    <span class="date">March 8, 2019</span>
-    <a class="title" href="/posts/math-typesetting/">Math Typesetting</a>
-  </li>
-  <li>
-    <span class="date">March 5, 2019</span>
-    <a class="title" href="/posts/emoji-support/">Emoji Support</a>
-  </li>
-</ul>
-~~~
+{{list_posts}}
--- utils.jl
+++ utils.jl
@@ -1,20 +1,67 @@
 using Dates
+import Hyperscript as HS
+
+node = HS.m
+
+# March 5, 2019
+dfmt(d) = Dates.format(d, "U d, yyyy")
 
 function hfun_dfmt(p::Vector{String})
     d = getlvar(Symbol(p[1]))
-    return "$(monthname(d)) $(day(d)), $(year(d))"
+    return dfmt(d)
 end
+
+# ===============================================
+# Logic to show the list of tags for a page
+# ===============================================
 
 function hfun_page_tags()
     tags = get_page_tags()
     base = globvar(:tags_prefix)
-    return join((
-        """
-        <span class="tag">
-          <a href="/$base/$id/">$name</a>
-        </span>
-        """
-        for (id, name) in tags),
-        """<span class="separator">•</span>"""
+    return join(
+        (
+            node("span", class="tag",
+                node("a", href="/$base/$id/", name)
+            )
+            for (id, name) in tags
+        ),
+        node("span", class="separator", "•")
+    )
+end
+
+# ===============================================
+# Logic to retrieve posts in posts/ and display
+# them as a list sorted by anti-chronological
+# order.
+#
+# Assumes that 'date' and 'title' are defined for
+# all posts.
+# ===============================================
+
+function hfun_list_posts()
+    return string(
+        node("ul",
+                (
+                    node("li",
+                        node("span", class="date", Dates.format(p.date, "U d, yyyy")),
+                        node("a", class="title", href=p.href, p.title)
+                    )
+                    for p in get_posts()
+                )...
+            )
         )
+end
+
+function get_posts()
+    paths = joinpath.("posts", filter!(p -> p != "index.md", readdir("posts")))
+    posts = [
+        (
+            date  = getvarfrom(:date, rp),
+            title = getvarfrom(:title, rp),
+            href  = "/$(splitext(rp)[1])"
+        )
+        for rp in paths
+    ]
+    sort!(posts, by = x->x.date, rev=true)
+    return posts
 end
